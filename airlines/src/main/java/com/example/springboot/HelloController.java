7152a44b0de9535a8dae16fff1b934619cc23de3
aninamu <anina.mu@grafana.com>
2024-12-06 13:27:37 -0500

    Add Swagger docs

--- airlines/src/main/java/com/example/springboot/HelloController.java
+++ airlines/src/main/java/com/example/springboot/HelloController.java
@@ -1,16 +1,19 @@
 package com.example.springboot;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Operation(summary = "Index", description = "No-op hello world")
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
 
+    @Operation(summary = "Health check", description = "Performs a simple health check")
 	@GetMapping("/health")
 	public String health() {
 		return "Health check passed!";
