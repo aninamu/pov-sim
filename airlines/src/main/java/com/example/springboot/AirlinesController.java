07967ffe2b2a6ce9886dd2024f693884b623c536
Brian Ashburn <bashburn@valesordev.com>
2025-01-24 10:06:05 -0800

    Fix formatting (#14)

--- airlines/src/main/java/com/example/springboot/AirlinesController.java
+++ airlines/src/main/java/com/example/springboot/AirlinesController.java
@@ -2,36 +2,34 @@ package com.example.springboot;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000") // Allow requests from React app
 public class AirlinesController {
-	private static String[] airlines = {"AA", "DL", "UA"};
+	private static String[] airlines = { "AA", "DL", "UA" };
 
-    @Operation(summary = "Index", description = "No-op hello world")
+	@Operation(summary = "Index", description = "No-op hello world")
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
 
-    @Operation(summary = "Health check", description = "Performs a simple health check")
+	@Operation(summary = "Health check", description = "Performs a simple health check")
 	@GetMapping("/health")
 	public String health() {
 		return "Health check passed!";
 	}
 
 	@GetMapping("/airlines")
-    @Operation(summary = "Get airlines", description = "Fetch a list of airlines")
-    public String getUserById(
-            @Parameter(description = "Optional flag - set raise to true to raise an exception")
-            @RequestParam(value = "raise", required = false, defaultValue = "false") boolean raise
-    ) {
+	@Operation(summary = "Get airlines", description = "Fetch a list of airlines")
+	public String getUserById(
+			@Parameter(description = "Optional flag - set raise to true to raise an exception") 
+			@RequestParam(value = "raise", required = false, defaultValue = "false") boolean raise) {
 		if (raise) {
 			throw new RuntimeException("Exception raised");
 		}
 		return String.join(", ", airlines);
-    }
+	}
 }
